e6b8f8ec1015a54cdad949a3d265bd167b7810fe
Aaron Barnes <aaron@io.nz>
2019-05-22 12:39:45 +1200

    Refactor to use new calendar API, restyle, remove weather

--- build.py
+++ build.py
@@ -1,70 +1,50 @@
 from __future__ import print_function
-import httplib2
-import os
-import re
-
-from apiclient import discovery
-import oauth2client
-from oauth2client import client
-from oauth2client import tools
-
 import datetime
-from pyquery import PyQuery
-from robobrowser import RoboBrowser
-import bleach
 import json
-import logging
 import os.path
-import requests
-import urllib
-import urlparse
+import pickle
+import pytz
+import re
+import subprocess
 
-import config
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request as gRequest
+import requests
+
+from config import CALENDARS, TIMEZONE, EVENT_REGEX
+
 
 # If modifying these scopes, delete your previously saved credentials
-# at ~/.credentials/calendar-python-quickstart.json
-SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-CLIENT_SECRET_FILE = 'client_secret.json'
-APPLICATION_NAME = 'Alfred'
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+CLIENT_SECRET_FILE = 'token.pickle'
+
 
 def get_credentials():
-    """Gets valid Google credentials from storage.
+    creds = None
 
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(CLIENT_SECRET_FILE):
+        with open(CLIENT_SECRET_FILE, 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(gRequest())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server()
+        # Save the credentials for the next run
+        with open(CLIENT_SECRET_FILE, 'wb') as token:
+            pickle.dump(creds, token)
 
-    Returns:
-        Credentials, the obtained credential.
-    """
-    home_dir = os.path.expanduser('~')
-    credential_dir = os.path.join(home_dir, '.credentials')
-    if not os.path.exists(credential_dir):
-        os.makedirs(credential_dir)
-    credential_path = os.path.join(credential_dir,
-                                   'calendar-python-quickstart.json')
+    return creds
 
-    store = oauth2client.file.Storage(credential_path)
-    credentials = store.get()
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        credentials = tools.run_flow(flow, store)
-        print('Storing credentials to ' + credential_path)
-    return credentials
-
-def get_page(url):
-    browser = RoboBrowser(user_agent='a python robot')
-    browser.open(url)
-    return PyQuery(browser.response.text)
-
-def make_url(base, partial):
-    if partial.startswith('http') or not len(base):
-        return partial
-
-    return urlparse.urljoin(base, partial)
 
 def get_git_revision_hash():
-    import subprocess
     return subprocess.check_output(['git', 'rev-parse', 'HEAD']).strip()
 
 
@@ -72,85 +52,39 @@ def get_git_revision_hash():
 # Get Google Calender entries
 ###
 credentials = get_credentials()
-http = credentials.authorize(httplib2.Http())
-service = discovery.build('calendar', 'v3', http=http)
+service = build('calendar', 'v3', credentials=credentials)
 
-now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
-eventsResult = service.events().list(
-    calendarId='primary', timeMin=now, maxResults=20, singleEvents=True,
-    orderBy='startTime').execute()
-eventsResult = eventsResult.get('items', [])
+# Call the Calendar API
+tz = pytz.timezone(TIMEZONE)
+midnight = tz.localize(datetime.datetime.combine(datetime.datetime.now(tz), datetime.time(0, 0)))
+startTimeObj = midnight.astimezone(pytz.utc)
+startTime = startTimeObj.isoformat()
+endTimeObj = startTimeObj + datetime.timedelta(4)
+endTime = endTimeObj.isoformat()
+
+#for c in service.calendarList().list().execute().get('items'):
+#    print(c)
+
 
 events = []
-for event in eventsResult:
-    start = event['start'].get('dateTime', event['start'].get('date'))
-    events.append((start, event['summary']))
+for title, calendar in CALENDARS.iteritems():
+    events_result = service.events().list(calendarId=calendar, timeMin=startTime, timeMax=endTime, maxResults=40, singleEvents=True, orderBy='startTime', timeZone='utc').execute()
+    eventsResult = events_result.get('items', [])
+
+    for event in eventsResult:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        end   = event['start'].get('dateTime', event['end'].get('date'))
+
+        etitle = title
+        for regex, newtitle in EVENT_REGEX:
+            if re.match(regex, event['summary']) is not None:
+                etitle = newtitle
+
+        events.append((start, end, event['summary'], etitle))
 
 
-###
-# Get weather
-###
-weather_current_url = "http://api.wunderground.com/api/{0}/conditions/q/{1}.json".format(config.WUNDERLAND_API, config.WUNDERLAND_LOCATION)
-r = urllib.urlopen(weather_current_url)
-weather_current = json.loads(r.read())['current_observation']
-
-weather = [
-    {
-        'day': 'Now'
-    },
-    {
-        'day': 'Today'
-    },
-    {
-        'day': 'Tonight'
-    },
-    {
-        'day': 'Tomorrow'
-    },
-    {
-        'day': 'Tomorrow night'
-    }
-]
-
-weather[0] = {
-    'day': 'Now',
-    'temperature': weather_current['temp_c'],
-    'forecast': weather_current['weather'],
-    'wind': str(weather_current['wind_kph']) + ' km/h ' + (weather_current['wind_dir'] if weather_current['wind_dir'] != 'Variable' else ''),
-    'icon': weather_current['icon']
-}
-
-weather_future_url = "http://api.wunderground.com/api/{0}/forecast/q/{1}.json".format(config.WUNDERLAND_API, config.WUNDERLAND_LOCATION)
-r = requests.get(weather_future_url)
-weather_future_json = r.json()
-weather_future_simple = weather_future_json['forecast']['txt_forecast']['forecastday']
-weather_future_detail = weather_future_json['forecast']['simpleforecast']['forecastday']
-
-i = 0
-for day in weather_future_simple:
-    i += 1
-    if i > 4:
-        break
-
-    detail = weather_future_detail[0 if i in (1,2) else 1]
-    icon = day['icon']
-    forecast = day['fcttext_metric']
-
-    # Remove temp
-    forecast = re.sub(r'(Low|High) ([^\.]+)?[0-9]+C\.(\s+)?', '', forecast)
-    wind = re.compile(r'Winds ([^\s]+) at ([^\.]+ km\/h)\.').search(forecast)
-    if wind:
-        forecast = re.sub(r'Winds [^\s]+ at [^\.]+ km\/h\.(\s+)?', '', forecast)
-        wind = '{0} {1}'.format(wind.group(2), wind.group(1)).replace(' to ', '-')
-    else:
-        wind = str(detail['maxwind']['kph']) + ' km/h ' + (detail['maxwind']['dir'] if detail['maxwind']['dir'] != 'Variable' else '')
-
-    temperature = detail['low']['celsius'] if i in (2,4) else detail['high']['celsius']
-
-    weather[i]['temperature'] = temperature
-    weather[i]['forecast'] = forecast.strip()
-    weather[i]['wind'] = wind.strip()
-    weather[i]['icon'] = icon
+# Resort the list of events by starttime now we have retreived from all calendars
+events = sorted(events, cmp=lambda x,y: cmp(x[0],y[0]))
 
 
 ###
@@ -159,23 +93,19 @@ for day in weather_future_simple:
 reddit_photos_url = "https://www.reddit.com/r/EarthPorn/top/.json?sort=top&t=day"
 r = requests.get(reddit_photos_url, headers={'User-Agent': 'https://github.com/srynot4sale/alfred'})
 photo_listings = r.json()
-if 'data' in photo_listings.keys():
-    for photo in photo_listings['data']['children']:
-        if photo['data']['domain'] != 'i.imgur.com':
-            continue
+for photo in photo_listings.get('data', {}).get('children', []):
+    if photo['data']['domain'] not in ('i.imgur.com', 'i.redd.it'):
+        continue
 
-        if photo['data']['over_18']:
-            continue
+    if photo['data']['over_18']:
+        continue
 
-        photo_url = photo['data']['url']
-        break
-else:
-    print(photo_listings)
+    photo_url = photo['data']['url']
+    break
 
 
 ret = {
     'cachebust': get_git_revision_hash(),
-    'weather': weather,
     'events': events,
     'background': photo_url
 }
